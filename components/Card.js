1f2a303178e3ad1ccb1228a9055ca1fe75a92ce9
Divine Orji <dpkreativ@gmail.com>
2021-07-21 05:44:32 +0100

    ui tweaks

--- components/Card.js
+++ components/Card.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Card = () => {
   return (
-    <div className="bg-white shadow-md p-6 w-96 rounded-md">
+    <div className="bg-white shadow-md p-6 w-full max-w-md rounded-lg">
       Hello, I'm a card
     </div>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,9 @@ import "../styles/globals.css";
 function MyApp({ Component, pageProps }) {
   return (
     <div className="bg-gray-200 min-h-screen">
+      <div className="max-w-4xl mx-auto">
       <Component {...pageProps} />
+      </div>
     </div>
   );
 }
