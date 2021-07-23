b6f1f3dbd58adb9dda60e54d590e7a3be18bad24
Divine Orji <dpkreativ@gmail.com>
2021-07-23 14:14:43 +0100

    add bg image to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        "contact-doodle":
+          "url(https://res.cloudinary.com/dpkreativ/image/upload/o_10/v1627045399/divi-crm/doodlebg_k5w7bx.png)",
+      }),
+    },
   },
   variants: {
     extend: {},
