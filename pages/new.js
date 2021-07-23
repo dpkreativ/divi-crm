67fa55558bb21ec3d8e27cdd65cf5ec30cccac7a
Divine Orji <dpkreativ@gmail.com>
2021-07-23 15:59:21 +0100

    test profile details component

---
+++ pages/new.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ProfileDetails from "../components/ProfileDetails";
+
+const NewContact = () => {
+  return (
+    <div>
+      <ProfileDetails />
+    </div>
+  );
+};
+
+export default NewContact;
