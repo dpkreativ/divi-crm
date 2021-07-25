62d9623d10f6abb5eed1492ae9470c78cef2c22c
Divine Orji <dpkreativ@gmail.com>
2021-07-25 04:28:12 +0100

    install and implement react-timeline-component

--- components/ProfileDetails.js
+++ components/ProfileDetails.js
@@ -1,5 +1,10 @@
 // import React from "react";
-import { MdEmail, MdPhone } from "react-icons/md";
+import { MdEmail, MdPhone, MdWork } from "react-icons/md";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
 
 const ProfileDetails = () => {
   return (
@@ -54,7 +59,46 @@ const ProfileDetails = () => {
             Add new entry
           </button>
         </div>
-        <div className="entries"></div>
+        <div className="entries">
+          <VerticalTimeline>
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work"
+              contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+              contentArrowStyle={{
+                borderRight: "7px solid  rgb(33, 150, 243)",
+              }}
+              iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+              icon={<MdWork />}
+            >
+              <h3 className="vertical-timeline-element-title">
+                Creative Director
+              </h3>
+              <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+              <p>
+                Creative Direction, User Experience, Visual Design, Project
+                Management, Team Leading
+              </p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work"
+              contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+              contentArrowStyle={{
+                borderRight: "7px solid  rgb(33, 150, 243)",
+              }}
+              iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+              icon={<MdWork />}
+            >
+              <h3 className="vertical-timeline-element-title">
+                Creative Director
+              </h3>
+              <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+              <p>
+                Creative Direction, User Experience, Visual Design, Project
+                Management, Team Leading
+              </p>
+            </VerticalTimelineElement>
+          </VerticalTimeline>
+        </div>
       </section>
     </div>
   );
