5e89a65552bc6cf9de63e3eb7387e0b11cf12c49
Divine Orji <dpkreativ@gmail.com>
2021-07-27 10:40:41 +0100

    Select first letter of last name to display

--- components/Card.js
+++ components/Card.js
@@ -1,6 +1,8 @@
 import Link from "next/link";
 
 const Card = ({ contact }) => {
+  const { firstname, lastname, phone } = contact.data;
+  const lastnameFirstLetter = lastname.split("")[0];
   return (
     <div className="bg-purple-100 w-36 rounded-xl m-6 flex flex-col pb-4 justify-between items-center">
       <div className="w-20 h-20 border-2 border-red-400 rounded-full -mt-10 mx-auto flex items-center justify-center p-1">
@@ -14,9 +16,9 @@ const Card = ({ contact }) => {
       </div>
       <div className="flex flex-col items-center">
         <div className="font-bold text-lg mt-2">
-          {contact.data.firstname} <span>O.</span>
+          {firstname} <span>{lastnameFirstLetter}.</span>
         </div>
-        <div>{contact.data.phone}</div>
+        <div>{phone}</div>
       </div>
 
       <Link href={`/profile/${contact.id}`}>
