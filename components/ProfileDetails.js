fc7c11a56f997783462003d49ff12d573c0aa675
Divine Orji <dpkreativ@gmail.com>
2021-07-27 09:33:29 +0100

    make profileDetails component dynamic

--- components/ProfileDetails.js
+++ components/ProfileDetails.js
@@ -1,4 +1,4 @@
-// import React from "react";
+import Link from "next/link";
 import { MdEmail, MdPhone, MdWork } from "react-icons/md";
 import {
   VerticalTimeline,
@@ -6,14 +6,14 @@ import {
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 
-const ProfileDetails = () => {
+const ProfileDetails = ({ contact }) => {
   return (
     <div>
       <div className="profile-header flex items-center rounded-full bg-doodle-1 bg-cover bg-center bg-gray-200 shadow-lg">
         <div className="w-24 h-24 md:w-28 md:h-28 lg:w-36 lg:h-36 border-2 border-gray-500 rounded-full flex items-center justify-center p-1 mr-4">
           <div className="w-full h-full bg-gray-600 rounded-full overflow-hidden">
             <img
-              src="https://res.cloudinary.com/dpkreativ/image/upload/v1616357794/IMG_-oi62yk_wtfop2.jpg"
+              src="https://res.cloudinary.com/dpkreativ/image/upload/o_20/v1627300464/divi-crm/Profile_avatar_placeholder_large_rbnj5w.png"
               alt="profile image"
               className="object-cover w-full h-full"
             />
@@ -21,9 +21,9 @@ const ProfileDetails = () => {
         </div>
         <div>
           <h1 className="font-bold text-2xl md:text-4xl text-gray-800">
-            Divine
+            {contact.data.firstname}
           </h1>
-          <p>Antetokounmpo</p>
+          <p>{contact.data.lastname}</p>
         </div>
       </div>
       <section className="mt-8">
@@ -31,26 +31,23 @@ const ProfileDetails = () => {
         <div className="mt-2 md:mt-4 grid md:grid-cols-2 gap-2">
           <div className="phone-number flex items-center">
             <MdPhone />
-            <div className="mx-2 text-lg">09021824073</div>
-            <button className="bg-green-700 text-white rounded-full py-1 px-4 text-xs ml-2">
-              call
-            </button>
+            <div className="mx-2 text-lg">{contact.data.phone}</div>
+            <Link href={`tel:${contact.data.phone}`}>
+              <button className="bg-green-700 text-white rounded-full py-1 px-4 text-xs ml-2">
+                call
+              </button>
+            </Link>
           </div>
           <div className="email flex items-center">
             <MdEmail />
-            <div className="mx-2 text-lg">dpkreativ@gmail.com</div>
+            <div className="mx-2 text-lg">{contact.data.mail}</div>
             <button className="bg-blue-700 text-white rounded-full py-1 px-4 text-xs ml-2">
               send mail
             </button>
           </div>
         </div>
         <h3 className="font-bold text-lg mt-2 md:mt-4">Bio</h3>
-        <div>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
-          expedita et necessitatibus, ipsam omnis consequatur cumque maxime,
-          illo in ea dolore soluta dignissimos suscipit, labore voluptatibus
-          recusandae. Magnam, quibusdam voluptas!
-        </div>
+        <div>{contact.data.bio}</div>
       </section>
       <section className="mt-8">
         <h2 className="font-bold text-xl md:text-2xl">Timeline</h2>
