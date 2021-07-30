3910565aa1e1cc6256df449930a819547d884acb
Divine Orji <dpkreativ@gmail.com>
2021-07-30 07:24:56 +0100

    ContactForm component works well

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,9 +1,66 @@
-import { Label, Input, TextArea } from "./FormComponents";
+import { useRouter } from "next/dist/client/router";
+import { Form, Label, Input, TextArea } from "./FormComponents";
 
 const ContactForm = () => {
+  const router = useRouter();
+
+  const createContact = async (data) => {
+    const { firstname, lastname, phone, mail, bio } = data;
+    try {
+      await fetch("/api/createContact", {
+        method: "POST",
+        body: JSON.stringify({ firstname, lastname, phone, mail, bio }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      router.push("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-    <form>
-      <div className="grid md:grid-cols-2 gap-4 mb-10">
+    <Form onSubmit={createContact}>
+      <Label htmlFor="firstname">First name</Label>
+      <Input
+        placeholder="Enter first name"
+        type="text"
+        name="firstname"
+        id="firstname"
+      />
+
+      <Label htmlFor="lastname">Last name</Label>
+      <Input
+        placeholder="Enter last name"
+        type="text"
+        name="lastname"
+        id="lastname"
+      />
+
+      <Label htmlFor="phone">Phone number</Label>
+      <Input
+        placeholder="Enter phone number"
+        type="tel"
+        name="phone"
+        id="phone"
+      />
+
+      <Label htmlFor="email">Email address</Label>
+      <Input
+        placeholder="Enter email address"
+        type="email"
+        name="mail"
+        id="mail"
+      />
+
+      <Label htmlFor="bio">Short Bio</Label>
+      <TextArea
+        placeholder="Write some key points to note about this particular contact"
+        name="bio"
+        id="bio"
+      />
+
+      {/* <div className="grid md:grid-cols-2 gap-4 mb-10">
         <div>
           <Label htmlFor="firstname">First name</Label>
           <Input
@@ -52,8 +109,8 @@ const ContactForm = () => {
             id="bio"
           />
         </div>
-      </div>
-      <div className="grid gap-4 mb-10">
+      </div> */}
+      <div className="grid gap-4 mb-10 mt-4">
         <button
           className="bg-green-500 hover:bg-green-700 text-white rounded-full p-4"
           type="submit"
@@ -61,7 +118,7 @@ const ContactForm = () => {
           Submit
         </button>
       </div>
-    </form>
+    </Form>
   );
 };
 
--- components/FormComponents.js
+++ components/FormComponents.js
@@ -1,3 +1,27 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+const CustomForm = ({ defaultValues, children, onSubmit }) => {
+  const methods = useForm({ defaultValues });
+  const { handleSubmit } = methods;
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {React.Children.map(children, (child) => {
+        return child.props.name
+          ? React.createElement(child.type, {
+              ...{
+                ...child.props,
+                register: methods.register,
+                key: child.props.name,
+              },
+            })
+          : child;
+      })}
+    </form>
+  );
+};
+
 const CustomLabel = ({ htmlFor, children }) => {
   return (
     <label htmlFor={htmlFor} className="block my-1.5">
@@ -6,34 +30,32 @@ const CustomLabel = ({ htmlFor, children }) => {
   );
 };
 
-const CustomInput = ({ type, name, id, placeholder, value, ref }) => {
+const CustomInput = ({ type, name, id, placeholder, register }) => {
   return (
     <input
+      {...register(name)}
       type={type}
-      name={name}
       id={id}
-      className="border-green-500 border-b-2 px-4 py-2 outline-none w-full shadow-lg"
+      className="border-green-500 border-b-2 px-4 py-2 mb-6 outline-none w-full shadow-lg"
       placeholder={placeholder}
-      value={value}
-      ref={ref}
     />
   );
 };
 
-const CustomTextArea = ({ placeholder, ref, name, id }) => {
+const CustomTextArea = ({ placeholder, register, name, id }) => {
   return (
     <textarea
       placeholder={placeholder}
-      className="w-full border-b-2 border-green-500 outline-none p-4 shadow-lg"
+      className="w-full border-b-2 border-green-500 outline-none p-4 mb-6 shadow-lg"
       rows="5"
-      ref={ref}
-      name={name}
+      {...register(name)}
       id={id}
     />
   );
 };
 
 export {
+  CustomForm as Form,
   CustomLabel as Label,
   CustomInput as Input,
   CustomTextArea as TextArea,
