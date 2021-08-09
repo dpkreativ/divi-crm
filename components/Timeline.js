91179f00ddcb2b673ba8926df9d8d38080d3c8ff
Divine Orji <dpkreativ@gmail.com>
2021-08-09 21:52:10 +0100

    tweak custom timeline component UI

--- components/Timeline.js
+++ components/Timeline.js
@@ -1,17 +1,22 @@
-import { MdDeleteForever, MdModeEdit } from "react-icons/md";
+import { MdChevronRight, MdDeleteForever, MdModeEdit } from "react-icons/md";
 
 const TimelineItem = ({ children, key }) => {
   return (
-    <div key={key}>
-      <div>{children}</div>
-      <div className="grid justify-items-end">
-        <div className="grid grid-cols-2 w-max gap-4 pt-4 text-xl">
-          <button>
-            <MdDeleteForever />
-          </button>
-          <button>
-            <MdModeEdit />
-          </button>
+    <div key={key} className="flex items-center mb-4">
+      <div className="border-2 -ml-3 text-sm rounded-full border-red-500 p-0.5 bg-white">
+        <MdChevronRight />
+      </div>
+      <div className="p-4 bg-gray-50 border-2 border-blue-800 ml-4 rounded-lg w-full shadow-lg">
+        <div>{children}</div>
+        <div className="grid justify-items-end">
+          <div className="grid grid-cols-2 w-max gap-4 pt-4 text-xl">
+            <button className="text-red-500">
+              <MdDeleteForever />
+            </button>
+            <button className="text-green-700">
+              <MdModeEdit />
+            </button>
+          </div>
         </div>
       </div>
     </div>
@@ -19,7 +24,7 @@ const TimelineItem = ({ children, key }) => {
 };
 
 const TimelineWrapper = ({ children }) => {
-  return <div>{children}</div>;
+  return <div className="mx-4 pt-8 border-l-2 border-blue-300">{children}</div>;
 };
 
 export { TimelineItem, TimelineWrapper };
